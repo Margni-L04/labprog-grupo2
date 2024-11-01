2d8af76267c1f8f68c0155b6251687f74b4bfc03
Margni-L04 <lucas.margni@est.fi.uncoma.edu.ar>
2024-11-01 18:59:09 -0300

    reacomodo de carpetas

--- TP 2/src/server.js
+++ TP 2/src/server.js
@@ -21,7 +21,7 @@ En la línea que contiene inet, buscamos la ip y la copiamos sin la máscara (17
 */
 
 const ip = '172.16.100.208';
-// const ip = 'localhost'; //para evitar usar el comando anterior
+//const ip = 'localhost'; //para evitar usar el comando anterior
 
 //elegimos un puerto, superior a 1024, pues estos son puertos reservados
 const puerto = '3017';
@@ -32,39 +32,39 @@ const path = require('path');
 //revisamos el file system para evitar poner la ruta correcta
 const fs = require('fs');
 
-//con el path, creamos la direccion donde se encuentra nuestro proyecto, que en este caso es volver uno para atrás
-const staticPath = path.join(__dirname, '..');
+//con el path, creamos la direccion donde se encuentra nuestro proyecto
+const staticPath = path.join(__dirname, '../public');
 //agregamos el path para el resto de páginas
-const staticPathPages = path.join(__dirname, '../pages');
+const staticPathPages = path.join(__dirname, '../public/pages');
 
 //imprimimos para observar si es la ruta que deseamos usar
 console.log('Ruta del proyecto:', staticPath);
 
-// con fs preguntamos si la ruta existe, de no ser asi, finalizamos el codigo
+//con fs preguntamos si la ruta existe, de no ser asi, finalizamos el codigo
 if (!fs.existsSync(staticPath)) {
     console.error('Ruta incorrecta :/ ', staticPath);
     process.exit(1);
 }
 
-// asignamos la direccion del index
+//asignamos la direccion del index
 const indexPath = path.join(staticPath, '/pages/index.html');
 
-// y con fs probamos si existe
+//con fs probamos si existe
 if (!fs.existsSync(indexPath)) {
     console.error('Archivo incorrecto: ', indexPath);
     process.exit(1);
 }
 
-// agregamos al servidor la ruta del proyecto y de las páginas
+//agregamos al servidor la ruta del proyecto y de las páginas
 app.use(express.static(staticPath));
 app.use(express.static(staticPathPages));
 
-//Cuando el usuario entre al server, se lo llevara al index
+//cuando el usuario entre al server, se lo llevara al index
 app.get('/', (req, res) => { // "/" indica la raiz
     res.sendFile(indexPath);
 });
 
-// abrimos el server en el puerto que especificamos
+//abrimos el server en el puerto que especificamos
 app.listen(puerto, () => {
     console.log('Entremos! http://' + ip + ':' + puerto);
 });
